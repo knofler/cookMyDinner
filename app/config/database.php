476006db0c4a5bfbb30a625c929a2df8f929dbe1
knofler <ahmed.rumman@gmail.com>
2014-06-06 03:03:33 +1000

    Add heroku creds and update default route to hit the DB

--- app/config/database.php
+++ app/config/database.php
@@ -1,13 +1,15 @@
 <?php
 
-return array(
-
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 	$host = $url["host"];
 	$username = $url["user"];
 	$password = $url["pass"];
 	$database = substr($url["path"], 1);
+
+return array(
+
+	
 	/*
 	|--------------------------------------------------------------------------
 	| PDO Fetch Style
